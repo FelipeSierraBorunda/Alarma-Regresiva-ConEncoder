a1fbc2f96d17a92bad1022641faa7e115a39495d
FelipeSierraBorunda <felipesierraborunda@gmail.com>
2025-03-14 18:42:01 -0600

    V2.0 Sin efecto antirebote

--- main.c
+++ main.c
@@ -21,7 +21,8 @@
 #define LedVerde GPIO_NUM_12
 #define Btn GPIO_NUM_13
 // Matriz de segmentos para los dígitos 0-9
-uint8_t Digitos[][7] = {
+uint8_t Digitos[][7] =
+ {
     {1,1,1,1,1,1,0}, // 0
     {0,1,1,0,0,0,0}, // 1
     {1,1,0,1,1,0,1}, // 2
@@ -34,11 +35,13 @@ uint8_t Digitos[][7] = {
     {1,1,1,0,0,1,1}  // 9
 };
 //Apagar
-void ApagarDisplays(void){
+void ApagarDisplays(void)
+{
     gpio_set_level(Catodo_2, 1);
     gpio_set_level(Catodo_3, 1);
 }
-void AsignarSegmentos(uint8_t BCD_Value){
+void AsignarSegmentos(uint8_t BCD_Value)
+{
 	gpio_set_level(Seg_a, Digitos[BCD_Value][0]);
 	gpio_set_level(Seg_b, Digitos[BCD_Value][1]);
 	gpio_set_level(Seg_c, Digitos[BCD_Value][2]);
@@ -81,7 +84,8 @@ void actualizar_display()
 	Display &= 1;
 }
 // Estado AlarmaDesarmada
-void AlarmaDesarmada(void* arg) {
+void AlarmaDesarmada(void* arg) 
+{
 		gptimer_handle_t timeralarma = (gptimer_handle_t) arg;
         EstadoRegresivo=false;
 		gpio_set_level(LedRojo, 1);
@@ -92,7 +96,8 @@ void AlarmaDesarmada(void* arg) {
 //================================================== interrupciones =================================================
 //================================================== Display
 // ISR para detectar dirección de giro
-static void IRAM_ATTR encoder_isr_handler(void* arg) {
+static void IRAM_ATTR encoder_isr_handler(void* arg) 
+{
     int estado_A = gpio_get_level(BtnA);
     int estado_B = gpio_get_level(BtnB);
     if (estado_A == estado_B) 
@@ -109,7 +114,8 @@ static void IRAM_ATTR encoder_isr_handler(void* arg) {
      }
 }
 // Interrupcion de Actualizacion de alarma
-static bool IRAM_ATTR ActualizacionDelDisplay(gptimer_handle_t timer, const gptimer_alarm_event_data_t *edata, void *user_data){
+static bool IRAM_ATTR ActualizacionDelDisplay(gptimer_handle_t timer, const gptimer_alarm_event_data_t *edata, void *user_data)
+{
 	BaseType_t high_task_awoken = pdFALSE;
 	actualizar_display();
 	return (high_task_awoken == pdTRUE); 		
@@ -132,13 +138,15 @@ static void IRAM_ATTR btn_isr_handler(void* arg)
 		AlarmaDesarmada(timeralarma);
 		gpio_isr_handler_add(BtnA, encoder_isr_handler, timeralarma);
 		gpio_isr_handler_add(Btn, btn_isr_handler,timeralarma);
+		CuentaBuzzer=0;
 		if(Cuenta==0){
 			Cuenta=1;
 		}
 	}
 }
 // Interrupcion de Actualizacion de alarma
-static bool IRAM_ATTR InterrupcionCuentaAbajo(gptimer_handle_t timer, const gptimer_alarm_event_data_t *edata, void *user_data){
+static bool IRAM_ATTR InterrupcionCuentaAbajo(gptimer_handle_t timer, const gptimer_alarm_event_data_t *edata, void *user_data)
+{
 	BaseType_t high_task_awoken = pdFALSE;
 	gpio_isr_handler_add(Btn, btn_isr_handler,(void*)timer);
  	if(Cuenta==0)
@@ -161,48 +169,65 @@ static bool IRAM_ATTR InterrupcionCuentaAbajo(gptimer_handle_t timer, const gpti
 	}
 	return (high_task_awoken == pdTRUE); 		
 }
-
 //================================================== Main ==========================================================
 void app_main(void)
 {
 // =========================================== 	Configuracion y habilitacion del timer ============================
-  //
   // Primero configuramos al timer de proposito general (64bits) para contar hacia arriba
-	gptimer_config_t TimerConfiguracionGeneral = {
+	gptimer_config_t TimerConfiguracionGeneral = 
+	{
 		.clk_src = GPTIMER_CLK_SRC_APB,			 
 		.direction = GPTIMER_COUNT_UP, 			
 		.resolution_hz = 1000000,				 
 	};
+	// Declaramos el manejador del gptimer llamado Timerdisplay
 	gptimer_handle_t TimerDisplay = NULL;		  	
-	
+	//Aplicamos Configuracion General
 	gptimer_new_timer(&TimerConfiguracionGeneral, &TimerDisplay);  
-
-	gptimer_alarm_config_t AlarmaDisplay = {
+	// Configuracion de la alarma
+	gptimer_alarm_config_t AlarmaDisplay = 
+	{
 		.alarm_count = 1000,
 		.reload_count = 0,
 		.flags.auto_reload_on_alarm = true
 	};
+	// Aplicamos la configuracion de la alarma para el manejador
 	gptimer_set_alarm_action(TimerDisplay, &AlarmaDisplay);
-
-	gptimer_event_callbacks_t CambioDeDisplay = {
+	// Establecemos que la alarma del manejador, active la siguiente interrupcion
+	gptimer_event_callbacks_t CambioDeDisplay = 
+	{
 		.on_alarm = ActualizacionDelDisplay,
 	};
+	// Aplicamos el evento al manejador
 	gptimer_register_event_callbacks(TimerDisplay, &CambioDeDisplay, NULL);
 	// Habilitaos y empezamos el timer del display
 	gptimer_enable(TimerDisplay);
 	gptimer_start(TimerDisplay);
 	//======================================== Timer de la alarma regresiva
-	gptimer_handle_t TimerAlarma = NULL;
+	// 
+	// 
+	//
+	//
+ //
+//dcsxzcxz
+ 	gptimer_handle_t TimerAlarma = NULL;
+ 	// Aplicamos la configuracion general al nuevo timer
 	gptimer_new_timer(&TimerConfiguracionGeneral, &TimerAlarma); 
-		gptimer_alarm_config_t AlarmaDescuenta = {
+	gptimer_alarm_config_t AlarmaDescuenta =
+	{
 		.alarm_count = 500000,
 		.reload_count = 0,
 		.flags.auto_reload_on_alarm = true
 	};
+	
+	// Aplicamos
 	gptimer_set_alarm_action(TimerAlarma, &AlarmaDescuenta);
-	gptimer_event_callbacks_t CuentaAbajo = {
+	// Establecemos Evento
+	gptimer_event_callbacks_t CuentaAbajo =
+	{
 		.on_alarm = InterrupcionCuentaAbajo,
 	};
+	// Aplicamos evento
 	gptimer_register_event_callbacks(TimerAlarma, &CuentaAbajo, NULL);
 	// Habilitaos y empezamos el timer del display
 	gptimer_enable(TimerAlarma);
@@ -221,19 +246,21 @@ void app_main(void)
     gpio_config(&ConfiguracionDeLasSalidas);
 
     // Configuración de entradas
-    gpio_config_t ConfiguracionDeLasEntradas = {
+    gpio_config_t ConfiguracionDeLasEntradas = 
+    {
         .pin_bit_mask = (1ULL << BtnA | 1ULL << BtnB | 1ULL << Btn),
         .mode = GPIO_MODE_INPUT,
         .pull_up_en = GPIO_PULLUP_ENABLE,
         .pull_down_en = GPIO_PULLDOWN_DISABLE,
         .intr_type = GPIO_INTR_NEGEDGE //dETECTAR CAMBIO DE ESTADO
     };
+    
     gpio_config(&ConfiguracionDeLasEntradas);
     // =============================================== VARIABLES DE INICIO =======================================
     //El Led rojo empieza encendido
     gpio_set_level(LedRojo, 1);
     //Pagardisplay 
-     ApagarDisplays();
+    ApagarDisplays();
     //Variable que acumula la cuenta
     Cuenta = 0;
     //Variable que dice en que display 
@@ -250,7 +277,6 @@ void app_main(void)
     {
     	// Delay
         vTaskDelay(1 / portTICK_PERIOD_MS);
-        
     }
   }
   
